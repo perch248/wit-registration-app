d4c0cc0838c5a152f52e221e18d5257d7a790206
Perch <percideguzman@gmail.com>
2025-08-04 21:15:14 -0600

    1st commit
    upload files

---
+++ register.php
@@ -0,0 +1,27 @@
+
+<?php
+$server = "tcp:wit-sqlsrv.database.windows.net,1433";
+$database = "WITStudentDB";
+$username = "dbadmin";
+$password = "P@ssword123";
+
+try {
+    $conn = new PDO("sqlsrv:server=$server;Database=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "INSERT INTO Students (FirstName, LastName, Email, Program)
+            VALUES (?, ?, ?, ?)";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([
+        $_POST["first_name"],
+        $_POST["last_name"],
+        $_POST["email"],
+        $_POST["program"]
+    ]);
+
+    echo "Registration successful!";
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+?>
